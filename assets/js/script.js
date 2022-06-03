7bcb68142f4319ae34e29887998467ab0a2c4eac
PGCranford <PGCranford@gmail.com>
2022-06-03 18:19:56 -0400

    setting grids

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,59 @@
-
+//set current day 
 var today =
-    moment().format("MMMM Do YYYY");
-moment().format("dddd");
+    moment().format("dddd MMMM Do YYYY HH:mm a");
 
-$("#currentDay").append(today)
+$("#currentDay").append(today);
+
+var hours = moment().format("HH:00");
+console.log(hours);
+//checking time of day for block
+
+var now = function () {
+    if ($("#dayHour") < hours) {
+        document.getElementById("textArea").setAttribute("past")
+
+
+    }
+    else ($("#dayHour") > today); {
+        $("#textArea").css(".future")
+    }
+};
+
+var addTask = function (taskText) {
+    var taskLi = $("#textArea");
+    .text()
+
+}
+
+now();
+
+
+
+
+
+
+
+
+
+
+
+// dayHour()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
